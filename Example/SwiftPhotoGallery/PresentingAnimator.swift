d2759c6c183683ce05cb1ed787f4ca487f90c93c
Ivan Matković <ivan.matkovic@cobeisfresh.com>
2018-05-09 17:24:43 +0200

    Added ability to set duration of DismissingAnimator and PresentingAnimator. Default value is set of 0.5

--- Example/SwiftPhotoGallery/PresentingAnimator.swift
+++ Example/SwiftPhotoGallery/PresentingAnimator.swift
@@ -11,23 +11,24 @@ import UIKit
 
 
 class PresentingAnimator: NSObject, UIViewControllerAnimatedTransitioning {
-
+    
     private let indexPath: IndexPath
     private let originFrame: CGRect
-    private let duration: TimeInterval = 0.5
-
-    init(pageIndex: Int, originFrame: CGRect) {
+    private let duration: TimeInterval
+    
+    init(pageIndex: Int, originFrame: CGRect, duration: TimeInterval = 0.5) {
         self.indexPath = IndexPath(item: pageIndex, section: 0)
         self.originFrame = originFrame
+        self.duration = duration
         super.init()
     }
-
+    
     func transitionDuration(using transitionContext: UIViewControllerContextTransitioning?) -> TimeInterval {
         return duration
     }
-
+    
     func animateTransition(using transitionContext: UIViewControllerContextTransitioning) {
-
+        
         guard let toView = transitionContext.view(forKey: .to),
             let fromVC = transitionContext.viewController(forKey: .from) as? MainCollectionViewController,
             let fromView = fromVC.collectionView?.cellForItem(at: indexPath) as? MainCollectionViewCell
@@ -35,25 +36,25 @@ class PresentingAnimator: NSObject, UIViewControllerAnimatedTransitioning {
                 transitionContext.completeTransition(true)
                 return
         }
-
+        
         let finalFrame = toView.frame
-
+        
         let viewToAnimate = UIImageView(frame: originFrame)
         viewToAnimate.image = fromView.imageView.image
         viewToAnimate.contentMode = .scaleAspectFill
         viewToAnimate.clipsToBounds = true
         fromView.imageView.isHidden = true
-
+        
         let containerView = transitionContext.containerView
         containerView.addSubview(toView)
         containerView.addSubview(viewToAnimate)
-
+        
         toView.isHidden = true
-
+        
         // Determine the final image height based on final frame width and image aspect ratio
         let imageAspectRatio = viewToAnimate.image!.size.width / viewToAnimate.image!.size.height
         let finalImageheight = finalFrame.width / imageAspectRatio
-
+        
         // Animate size and position
         UIView.animate(withDuration: duration, animations: {
             viewToAnimate.frame.size.width = finalFrame.width
@@ -65,6 +66,6 @@ class PresentingAnimator: NSObject, UIViewControllerAnimatedTransitioning {
             viewToAnimate.removeFromSuperview()
             transitionContext.completeTransition(true)
         })
-
+        
     }
 }
